74ae719b4ed06dc611dfc7cba5c4bd4310816879
wagagaha <onetime-depths0l@icloud.com>
2023-11-24 16:29:01 +0800

    chore: add homebrew config

---
+++ homebrew/config.zsh
@@ -0,0 +1,6 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+export HOMEBREW_DIR="$(brew --cellar)"
