768a6dec15eb45414b02fb6c4bdeab023a026c25
wagagaha <onetime-depths0l@icloud.com>
2023-12-14 15:16:20 +0800

    chore: update alias

--- zsh/public.zsh
+++ zsh/public.zsh
@@ -1,6 +1,6 @@
 # startship
-export STARSHIP_CONFIG="$HOME/.config/starship/zsh.toml"
-eval "$(starship init zsh)"
+#export STARSHIP_CONFIG="$HOME/.config/starship/zsh.toml"
+#eval "$(starship init zsh)"
 
 # >> alias
 # main
@@ -17,6 +17,9 @@ alias gam="export GPG_TTY=$(tty) && git commit -am"
 alias gck='git checkout'
 alias ga="git add"
 
+# update
+alias up="bash $HOME/.config/plum/rime-install iDvel/rime-ice:others/recipes/full && brew upgrade && brew cleanup"
+
 # >> clean
 # Delete Apple System Logs
 alias dasl="sudo rm -rf /private/var/log/asl/*.asl"
@@ -30,8 +33,13 @@ alias dspm="rm -rf $HOME/Library/Caches/org.swift.swiftpm/;rm -rf $HOME/Library/
 # Flush Directory Service cache
 alias flushdns="dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
 
+# Rebuild Launch Services database
+alias rlsd="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
 
-alias clean="dasl && ded && flushdns && dspm"
+alias clean="dasl &&\
+  ded &&\
+  flushdns &&\
+  dspm"
 
 # >> Others
 # local ip
